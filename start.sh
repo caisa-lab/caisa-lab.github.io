1bed51e4d991f26adcf8fae82f9bcb8b173f6f1f
Wei-Fan Chen <viericwf@gmail.com>
2024-09-12 16:49:37 +0200

    update theses

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
+export RUBY_HOME=/home/cwf/.local/share/gem/ruby/3.0.0/bin
+export PATH="$RUBY_HOME:$PATH"
+export GEM_HOME=$HOME/.gem
 bundle
 bundle exec jekyll serve --open-url --livereload --trace
