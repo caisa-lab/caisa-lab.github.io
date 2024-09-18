b9d4f19b3ae69bbfc9dbe7fbd52dfc009793bd2b
Wei-Fan Chen <viericwf@gmail.com>
2024-09-18 09:43:55 +0200

    update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export RUBY_HOME=/home/cwf/.local/share/gem/ruby/3.0.0/bin
+export RUBY_HOME=$HOME/.local/share/gem/ruby/3.0.0/bin
 export PATH="$RUBY_HOME:$PATH"
 export GEM_HOME=$HOME/.gem
 bundle
